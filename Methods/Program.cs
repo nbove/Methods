cebbc68bdc55b6c27e9192bb77dfa7911b38c648
Nick <nbove@mail.usf.edu>
2019-01-31 23:29:30 -0500

    version1

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/* Author: Nick Bove
+   Date: 1/31/2019
+   Comments: This C# Console application code demonstrates the use of methods
+*/
+using System;
 
 namespace Methods
 {
@@ -6,7 +10,26 @@ namespace Methods
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Please enter your name");
+
+			try
+			{
+				string input = Console.ReadLine();
+				int value_of_input = int.Parse(input);
+
+				int get_user_name = User_Name(value_of_input);
+				Recommend_Exercise(get_user_name);
+			}
+			catch
+			{
+				Console.WriteLine("Press any key to exit the program..");
+			}
+		}
+
+		private static int User_Name(int get_name);
+		{
+		int dispaly_name = get_name;
+		return display_name;
 		}
 	}
 }
